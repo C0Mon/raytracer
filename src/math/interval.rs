309586e7a9d9fe96768210ca7520ca2b9006337d
Null <colm.monahan23@gmail.com>
2026-01-17 03:42:15 +0000

    feat: add interval struct

---
+++ src/math/interval.rs
@@ -0,0 +1,65 @@
+use std::f64;
+
+struct Interval {
+    min: f64,
+    max: f64,
+}
+
+impl Interval {
+    pub fn new(min: f64, max: f64) -> Self {
+        Self { min, max }
+    }
+    pub fn size(&self) -> f64 {
+        self.max - self.min
+    }
+    pub fn contains(&self, x: f64) -> bool {
+        self.min <= x && x <= self.max
+    }
+    pub fn surround(&self, x: f64) -> bool {
+        self.min < x && x < self.max
+    }
+    pub fn empty() -> Interval {
+        Interval::new(f64::INFINITY, -f64::INFINITY)
+    }
+    pub fn universe() -> Interval {
+        Interval::new(-f64::INFINITY, f64::INFINITY)
+    }
+}
+
+impl Default for Interval {
+    fn default() -> Self {
+        Interval::empty()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+    
+    #[rstest]
+    #[case(Interval::new(3.0, 5.0), 2.0)]
+    #[case(Interval::new(3.0, 5.0), 2.0)]
+    #[case(Interval::new(0.0, 0.0), 0.0)]
+    #[case(Interval::new(1.0, 0.0), -1.0)]
+    fn test_size(#[case] a: Interval, #[case] expected: f64) {
+        assert_eq!(a.size(), expected);
+    }
+    #[rstest]
+    #[case(Interval::new(3.0, 5.0), 4.0, true)]
+    #[case(Interval::new(3.0, 5.0), 5.0, true)]
+    #[case(Interval::new(0.0, 0.0), 0.0, true)]
+    #[case(Interval::new(1.0, 0.0), -1.0, false)]
+    fn test_contains(#[case] a: Interval, #[case] b: f64, #[case] expected: bool) {
+        assert_eq!(a.contains(b), expected);
+    }
+    #[rstest]
+    #[case(Interval::new(3.0, 5.0), 4.0, true)]
+    #[case(Interval::new(3.0, 5.0), 5.0, false)]
+    #[case(Interval::new(0.0, 0.0), 0.0, false)]
+    #[case(Interval::new(1.0, 0.0), -1.0, false)]
+    fn test_surrounds(#[case] a: Interval, #[case] b: f64, #[case] expected: bool) {
+        assert_eq!(a.surround(b), expected);
+    }
+    
+}
