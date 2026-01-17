2c3b1047b4c91c6ba94fd88f14f3a61d4cb4e6c1
Null <colm.monahan23@gmail.com>
2026-01-17 04:54:02 +0000

    feat: add math utils

--- src/math.rs
+++ src/math.rs
@@ -1,2 +1,3 @@
 pub mod vector;
 pub mod interval;
+pub mod util;
---
+++ src/math/util.rs
@@ -0,0 +1,12 @@
+use std::f64::consts::PI;
+pub fn degrees_to_radians(degrees: f64) -> f64 {
+    (degrees * PI) / 100.0
+}
+
+pub fn random_normalised() -> f64 {
+    rand::random_range(0.0..=1.0)
+}
+
+pub fn random_float(min: f64, max: f64) -> f64 {
+    rand::random_range(min..=max)
+}
